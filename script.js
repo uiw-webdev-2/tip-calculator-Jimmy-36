c3b4022de429ed7260b85d3e262652380d7adf46
Jimmy-36 <89508072+Jimmy-36@users.noreply.github.com>
2022-04-14 17:35:01 -0500

    Update
    Update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
- const formatter = (locale = "en-US", currency = "USD", value) => {
+ const formatter = (locale, currency, value) => {
     let formattedValue = new Intl.NumberFormat(locale, {
       style: "currency",
       currency: currency,
@@ -15,18 +15,30 @@
   
     return formattedValue;
   };
-  
+
   const tipCalculator = (sum, percentage, locale, currency) => {
     let tip = sum * (percentage / 100);
     let total = sum + tip;
-  
+    
     console.log(`
       Sum before tip: ${formatter(locale, currency, sum)}
       Tip percentage: ${percentage}%
       Tip:            ${formatter(locale, currency, tip)}
       Total:          ${formatter(locale, currency, total)}
     `);
+    alert(`This is your total after the tip: ${formatter(locale, currency, total)}`);
   };
-  
-  tipCalculator(29.95, 18, "en", "USD");
-  
+ 
+
+  document.getElementById("myBtn").onclick = function() { 
+    let amount = document.getElementById("amount").value;
+    let tipPercent = document.getElementById("tipPercent").value;
+    
+    var skillsSelect = document.getElementById("select");
+    var skillsValue = document.getElementById("select").value;
+    var selectedText = skillsSelect.options[skillsSelect.selectedIndex].text;
+
+    tipCalculator(amount, tipPercent, skillsValue, selectedText);
+  };
+ 
+//lingo.net iso code language table, putting in the new country will overwrite the default for the US
